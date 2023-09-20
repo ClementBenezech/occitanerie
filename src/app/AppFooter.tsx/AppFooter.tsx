fa4cc4ae7d2db6b9c68aede941903d0ec68011ea
Clement Benezech <clement.b@obat.fr>
2023-09-20 11:44:18 +0200

    more modifications yeah

--- src/app/AppFooter.tsx/AppFooter.tsx
+++ src/app/AppFooter.tsx/AppFooter.tsx
@@ -1,9 +1,9 @@
 const AppFooter = () => {
   return (
     <div className="footer">
-      <div className="footer-container-1">Some Information</div>
-      <div className="footer-container-2">
-        <div>${"L'occitanerie"}</div>
+      <div className="footer-container">Some Information</div>
+      <div className="footer-container">
+        <div>L'occitanerie</div>
         <div>Worlds best ferme pédagogique since 1845</div>
         <div>669 chemin des camparnals</div>
         <div>31340 le born</div>
--- src/app/home/page.tsx
+++ src/app/home/page.tsx
@@ -1,5 +1,6 @@
 /* eslint-disable react/no-unescaped-entities */
-import homeImg from "../../images/super donkeys.jpg";
+import homeImg from "../../images/farmers.jpg";
+import donkeys from "../../images/tirelalangue.jpg";
 import styles from "./../page.module.css";
 import Image from "next/image";
 
@@ -26,6 +27,38 @@ export default function Home() {
           La structure permet l’accueil de petits groupes, toute l’année.
         </span>
         <span className="standard-paragraph">Uniquement sur réservation.</span> */}
+      </div>
+      <div className="page-content">
+        <div className="standard-paragraph black">
+          Tu vois, premièrement, entre penser et dire, il y a un monde de
+          différence et c'est très, très beau d'avoir son propre moi-même !
+          C'est cette année que j'ai eu la révélation ! Tu vois, je sais que,
+          grâce à ma propre vérité il faut toute la splendeur du aware et
+          parfois c'est bon parfois c'est pas bon. Et tu as envie de le dire au
+          monde entier, including yourself
+        </div>{" "}
+        <div className="standard-paragraph black">
+          {" "}
+          Si je t'emmerde, tu me le dis, après il faut s'intégrer tout ça dans
+          les environnements et il y a de bonnes règles, de bonnes rules car
+          l'aboutissement de l'instinct, c'est l'amour !
+        </div>{" "}
+        <Image
+          className="standard-content-image"
+          src={donkeys}
+          alt="Des ânes contents qui tirent la langue"
+        />
+        <div className="standard-paragraph black">
+          {" "}
+          Mais ça, c'est uniquement lié au spirit. Si je t'emmerde, tu me le
+          dis, si vraiment tu veux te rappeler des souvenirs de ton perroquet,
+          c'est un très, très gros travail car l'aboutissement de l'instinct,
+          c'est l'amour ! Et tu as envie de le dire au monde entier, including
+          yourself. Quand tu fais le calcul, là on voit qu'on a beaucoup à
+          travailler sur nous-mêmes car entre penser et dire, il y a un monde de
+          différence et ça, c'est très dur, et, et, et... c'est très facile en
+          même temps. C'est cette année que j'ai eu la révélation !
+        </div>
       </div>
     </main>
   );
