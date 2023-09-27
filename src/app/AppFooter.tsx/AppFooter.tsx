1bf1b529264655071369c4ca4b03d8271e8c673b
Clement Benezech <clement.b@obat.fr>
2023-09-27 13:00:39 +0700

    fixing un-escaped quote

--- src/app/AppFooter.tsx/AppFooter.tsx
+++ src/app/AppFooter.tsx/AppFooter.tsx
@@ -3,7 +3,7 @@ const AppFooter = () => {
     <div className="footer">
       <div className="footer-container">Some Information</div>
       <div className="footer-container">
-        <div>L'occitanerie</div>
+        <div>Occitanerie</div>
         <div>Worlds best ferme pédagogique since 1845</div>
         <div>669 chemin des camparnals</div>
         <div>31340 le born</div>
