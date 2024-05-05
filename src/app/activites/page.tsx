07a9e6426bbc63057b3b79061222138e3351bb6f
Clement Benezech <clement.b@obat.fr>
2024-05-05 16:51:12 +0700

    fixing layout and improving page title designs

--- src/app/activites/page.tsx
+++ src/app/activites/page.tsx
@@ -33,30 +33,32 @@ export default function Page() {
         src={animals}
         alt="Des bons gros farmers"
       />
-      <svg
-        className="cover-text-background"
-        xmlns="http://www.w3.org/2000/svg"
-        viewBox="0 0 1440 320"
-      >
-        <path
-          fill="#000000ac"
-          fill-opacity="0.8"
-          d="M0,160L120,138.7C240,117,480,75,720,85.3C960,96,1200,160,1320,192L1440,224L1440,320L1320,320C1200,320,960,320,720,320C480,320,240,320,120,320L0,320Z"
-        ></path>
-      </svg>
-      <div className="page-cover-image-text">
-        <div className="standard-section-title">Des activités pour tous!</div>
-        <span className="cover-paragraph">
-          Notre objectif premier est de mettre en lien les visiteurs avec les
-          animaux de la ferme mais aussi les végétaux grâce à la serre, au
-          potager et au verger.
-        </span>
-        <span className="cover-paragraph">
-          Nous proposons de nombreuses activités autour des animaux, du
-          maraîchage et du “travail” à la ferme en général. Différents ateliers
-          sont également proposés tous les mois, en fonction des saisons et de
-          ce que nous offre la ferme et la nature.
-        </span>
+      <div className="title-container">
+        <svg
+          className="cover-text-background"
+          xmlns="http://www.w3.org/2000/svg"
+          viewBox="0 0 1440 320"
+        >
+          <path
+            fill="#000000ac"
+            fill-opacity="0.8"
+            d="M0,160L120,138.7C240,117,480,75,720,85.3C960,96,1200,160,1320,192L1440,224L1440,320L1320,320C1200,320,960,320,720,320C480,320,240,320,120,320L0,320Z"
+          ></path>
+        </svg>
+        <div className="page-cover-image-text">
+          <div className="standard-section-title">Des activités pour tous!</div>
+          <span className="cover-paragraph">
+            Notre objectif premier est de mettre en lien les visiteurs avec les
+            animaux de la ferme mais aussi les végétaux grâce à la serre, au
+            potager et au verger.
+          </span>
+          <span className="cover-paragraph">
+            Nous proposons de nombreuses activités autour des animaux, du
+            maraîchage et du “travail” à la ferme en général. Différents
+            ateliers sont également proposés tous les mois, en fonction des
+            saisons et de ce que nous offre la ferme et la nature.
+          </span>
+        </div>
       </div>
       {ActivitiesCards}
     </main>
