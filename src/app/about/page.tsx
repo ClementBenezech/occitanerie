e9363a132000d06d600b87c5f67fc790ee873b8e
Clement Benezech <clement.b@obat.fr>
2024-05-26 16:03:57 +0700

    adding social networks feeds as Iframes for now

--- src/app/about/page.tsx
+++ src/app/about/page.tsx
@@ -17,6 +17,24 @@ export default function Page() {
         src={farmMap}
         alt="Un plan qui montre les différentes zones de la ferme pédagogique"
       />
+      <div className={styles.flexrow}>
+        <iframe
+          width="320"
+          height="400"
+          src="https://www.instagram.com/l.occit.anerie/embed/"
+          frameBorder="0"
+        ></iframe>
+        <iframe
+          src="https://www.facebook.com/plugins/page.php?href=https%3A%2F%2Fwww.facebook.com%2FLoccitanerie&tabs=timeline%2C%20events&width=500&height=900&small_header=true&adapt_container_width=true&hide_cover=true&show_facepile=false&appId"
+          width="500"
+          height="900"
+          style={{ border: "none", overflow: "hidden" }}
+          scrolling="no"
+          frameBorder="0"
+          allowFullScreen={true}
+          allow="autoplay; clipboard-write; encrypted-media; picture-in-picture; web-share"
+        ></iframe>
+      </div>
     </main>
   );
 }
