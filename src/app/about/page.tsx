594f726b81c5e1c8db807391bca666d2a5fd0add
Clement Benezech <clement.b@obat.fr>
2024-05-26 16:08:01 +0700

    less wide facebook widget

--- src/app/about/page.tsx
+++ src/app/about/page.tsx
@@ -26,7 +26,7 @@ export default function Page() {
         ></iframe>
         <iframe
           src="https://www.facebook.com/plugins/page.php?href=https%3A%2F%2Fwww.facebook.com%2FLoccitanerie&tabs=timeline%2C%20events&width=500&height=900&small_header=true&adapt_container_width=true&hide_cover=true&show_facepile=false&appId"
-          width="500"
+          width="400"
           height="900"
           style={{ border: "none", overflow: "hidden" }}
           scrolling="no"
